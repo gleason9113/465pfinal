a7acaeafc0a7b2a0b4f461f3a254fa630df84202
Amir <a.basirat.67@gmail.com>
2023-05-24 22:17:02 -0700

    first views styling

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,11 +1,21 @@
-import Home from './components/Home';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+import MainView from "./components/MainView/MainView";
+import DetailedView from "./components/DetailedView/DetailedView";
+import HistoricalView from "./components/HistoricalView/HistoricalView";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/detailed" element={<DetailedView />} />
+        <Route path="/historical" element={<HistoricalView />} />
+        <Route path="/" element={<MainView />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ dashboard/src/components/MainView/MainView.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./MainView.css";
+import MapChart from "../Map/Map";
+
+const MainView = () => {
+  // Sample data to be passed to the Map component
+  const countries = [
+    { country: "Country A", pm10: 25 },
+    { country: "Country B", pm10: 40 },
+    { country: "Country C", pm10: 10 },
+    // Add more country data as needed
+  ];
+
+  return (
+    <div className="main-view">
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/">Main</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/detailed">Detailed</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/historical">Historical</Link>
+          </li>
+        </ul>
+      </nav>
+      <h1>Air Quality Dashboard</h1>
+      <p>
+        Welcome to the Air Quality Dashboard. Select a page from the navigation
+        bar above to explore more.
+      </p>
+      <div className="main-map-container">
+        <MapChart />
+      </div>
+      <div className="search-box">
+        <input type="text" placeholder="Search city..." />
+        <button className="search-btn">Search</button>
+      </div>
+    </div>
+  );
+};
+
+export default MainView;
