4c63b9a51c96080007f0bd99f7575e384ea62fc5
fr0sty <gleason.ariel@gmail.com>
2023-06-01 18:43:36 -0700

    Reverted detailed view

--- dashboard/src/components/DetailedView/DetailedView.js
+++ dashboard/src/components/DetailedView/DetailedView.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 import "./DetailedView.css";
 import CityMap from "../Map/CityMap";
-import { getAllCities, getCityData, getCountryData } from '../../api';
 
 const DetailedView = () => {
   // New York City, USA: [40.7128, -74.0060]
@@ -17,44 +16,6 @@ const DetailedView = () => {
   // Moscow, Russia: [55.7558, 37.6176]
   // Rio de Janeiro, Brazil: [-22.9068, -43.1729]
   const cityPosition = [34.0522, -118.2437];
-  const [search, setSearch] = useState('');
-  const [results, setResults] = useState([]);
-  const [searchType, setSearchType] = useState('city');
-
-  const handleSearchChange = (e) => {
-    setSearch(e.target.value);
-  };
-
-  const handleSearchClick = async () => {
-    let data;
-    console.log(search);
-    console.log(searchType);
-    try {
-      let testData = await getAllCities();
-      console.log(testData);
-      if (searchType === 'city') {
-        console.log('Calling City Search...');
-        data = await getCityData(search);
-      } else {
-        console.log('Calling Country Search...');
-        data = await getCountryData(search);
-      }
-      setResults(data); 
-      console.log("View Data:", data); 
-    } catch (error) {
-      console.error(`An error occurred: ${error}`);
-    }
-  };
-
-  const handleClearClick = () => {
-    setSearch('');
-  };
-
-  const handleSearchTypeChange = (e) => {
-    setSearchType(e.target.value);
-  };
-
-
 
   return (
     <div className="detailed-view">
@@ -72,20 +33,6 @@ const DetailedView = () => {
         </ul>
       </nav>
       <h1>Detailed View</h1>
-      <div className="search-bar">
-        <input 
-          type="text" 
-          value={search} 
-          onChange={handleSearchChange} 
-          placeholder="Search..." 
-        />
-         <div>
-          <input type="radio" value="city" checked={searchType === 'city'} onChange={handleSearchTypeChange} /> City
-          <input type="radio" value="country" checked={searchType === 'country'} onChange={handleSearchTypeChange} /> Country
-        </div>
-        <button onClick={handleSearchClick}>Search</button>
-        <button onClick={handleClearClick}>Clear</button>
-      </div>
       <div className="detailed-map-container">
         <CityMap position={cityPosition} />
       </div>
