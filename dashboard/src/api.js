9084f5eeaa82125443c265505b7b6c418fb1b0dc
frosty <gleas3@pdx.edu>
2023-06-06 09:33:02 -0700

    Fixed getCountryData and getCityData to pull measurement data

--- dashboard/src/api.js
+++ dashboard/src/api.js
@@ -19,40 +19,10 @@ export const getCountries = async () => {
 
 export const getCityData = async (city) => {
   try {
-    const url = `https://api.openaq.org/v2/cities?city=${encodeURIComponent(city)}`;
+    const url = `https://api.openaq.org/v2/latest?city=${encodeURIComponent(city)}`;
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Failed to fetch data for ${city}: ${response.status}  ${response.statusText}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(`An error occurred: ${error}`);
-    throw error;
-  }
-}
-
-export const getLatestCityData = async (city) => {
-  try {
-    const url = `https://api.openaq.org/v2/latest?limit=1&city=${encodeURIComponent(city)}`;
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data for ${city}: ${response.status}  ${response.statusText}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(`An error occurred: ${error}`);
-    throw error;
-  }
-}
-
-export const getLatestCountryData = async (country) => {
-  try {
-    const url = `https://api.openaq.org/v2/latest?limit=1&country=${encodeURIComponent(country)}`;
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data for ${country}: ${response.status}  ${response.statusText}`);
     }
     const data = await response.json();
     return data;
@@ -64,13 +34,15 @@ export const getLatestCountryData = async (country) => {
 
 export const getCountryData = async (country) => {
   try {
-    const url = 'https://api.openaq.org/v2/countries?limit=200';
+    const countryCode = await getCountryCode(country);
+    const url = `https://api.openaq.org/v2/latest?limit=500&country=${countryCode}`;
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Failed to fetch data for ${country}: ${response.status}  ${response.statusText}`);
     }
     const data = await response.json();
-    const targetCountry = data.results.filter((countryData) => countryData.name === country);
+    const targetCountry = data.results.find((countryData) => countryData.country === countryCode);
+    console.log(targetCountry);
     return targetCountry;
   } catch (error) {
     console.log(`An error occurred: ${error}`);
@@ -80,9 +52,7 @@ export const getCountryData = async (country) => {
 
 export const getDateRange = async (startDate, endDate, location, type) => {
   try {
-    // construct the URL
     const url = `https://api.openaq.org/v2/measurements?date_from=${encodeURIComponent(startDate)}&date_to=${encodeURIComponent(endDate)}&${encodeURIComponent(type)}=${encodeURIComponent(location)}`;
-
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Failed to fetch data for ${location} between ${startDate} and ${endDate}: ${response.status}  ${response.statusText}`);
@@ -120,3 +90,29 @@ export async function getAllPollutants() {
   const allPollutants = await apiClient.get("/parameters?order_by=name");
   return allPollutants.data;
 }
+
+export async function getCountryCode(country) {
+  try {
+    const url = 'https://api.openaq.org/v2/countries?limit=200';
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch country data: ${response.status} ${response.statusText}`);
+    }
+    const data = await response.json();
+    const target = data.results.find((countryData) => countryData.name === country);
+    if (!target) {
+      throw new Error('Country not found');
+    }
+
+    return target.code;
+  } catch (error) {
+    console.log(`An error occurred: ${error}`);
+    throw error;
+  }
+}
+
+
+
+
+
+//Sources, so I don't forget later: https://www.airnow.gov/sites/default/files/2020-05/aqi-technical-assistance-document-sept2018.pdf, https://forum.airnowtech.org/t/the-aqi-equation/169
