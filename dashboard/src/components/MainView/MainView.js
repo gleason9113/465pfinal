bba3949b9b9a8da96e7ab64363477bc3d3f65f05
Amir <a.basirat.67@gmail.com>
2023-06-06 18:20:14 -0700

    fixed some minor errors

--- dashboard/src/components/MainView/MainView.js
+++ dashboard/src/components/MainView/MainView.js
@@ -8,32 +8,36 @@ import PollutantDetails from "../Pollutants/PollutantDetails";
 import TopCountries from "../TopCountries/TopCountries";
 import { getCityData, getCountryData } from "../../api";
 
-
 const MainView = ({ allPollutants = [] }) => {
   const navigate = useNavigate();
   const [selectedPollutant, setSelectedPollutant] = useState("");
   const [searchValue, setSearchValue] = useState("");
   const [searchType, setSearchType] = useState("");
 
-
   const searchOptions = [
-    { value: '', label: 'Select search parameter' },
-    { value: 'city', label: 'City' },
-    { value: 'country', label: 'Country' },
+    { value: "", label: "Select search parameter" },
+    { value: "city", label: "City" },
+    { value: "country", label: "Country" },
   ];
 
   const onSearchButtonClick = async () => {
+    navigate("/detailed", {
+      state: {
+        allPollutants,
+        searchedValue: searchValue,
+        searchedType: searchType,
+      },
+    });
 
-    const countryResponse = await getCountryData("Mexico");
-    console.log(countryResponse);
-    const cityResponse = await getCityData("London");
-    console.log(cityResponse);
-    const result = await getCityData(searchedCity)
-      .then(response => response.results);
-    setCityData(result[0]); //getCityData will now return either an element of the results array (station w the highest # of recorded parameters)
-    //or the response as a whole if the results array is empty or missing - this needs to be adjusted as a result.
-  }
-
+    // const countryResponse = await getCountryData("Mexico");
+    // console.log(countryResponse);
+    // const cityResponse = await getCityData("London");
+    // console.log(cityResponse);
+    // const result = await getCityData(searchedCity)
+    //   .then(response => response.results);
+    // setCityData(result[0]); //getCityData will now return either an element of the results array (station w the highest # of recorded parameters)
+    // //or the response as a whole if the results array is empty or missing - this needs to be adjusted as a result.
+  };
 
   return (
     <div className="main-view">
@@ -62,17 +66,25 @@ const MainView = ({ allPollutants = [] }) => {
         </div>
 
         <div className="main-map-container">
-
           <div className="search-box detail-search-box">
-            <select value={searchType} onChange={handleChange}>
+            {/* <select value={searchType} onChange={handleChange}>
               {searchOptions.map(option => (
                 <option key={option.value} value={option.value}>
                   {option.label}
                 </option>
               ))}
-            </select>
-            <input id="cityName" name="cityName" value={searchValue} onChange={e => setSearchValue(e.target.value)} type="text" placeholder="Search..." />
-            <button className="search-btn" onClick={onSearchButtonClick}>Search</button>
+            </select> */}
+            <input
+              id="cityName"
+              name="cityName"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              type="text"
+              placeholder="Search..."
+            />
+            <button className="search-btn" onClick={onSearchButtonClick}>
+              Search
+            </button>
           </div>
           <MapChart />
         </div>
